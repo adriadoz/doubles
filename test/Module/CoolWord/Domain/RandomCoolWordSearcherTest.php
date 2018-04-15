78abe0fe162baa2f43c7e76d519eeca8e217c493
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-04-15 14:03:59 +0200

    Test behavior for RandomCoolWordSearcher

--- test/Module/CoolWord/Domain/RandomCoolWordSearcherTest.php
+++ test/Module/CoolWord/Domain/RandomCoolWordSearcherTest.php
@@ -4,12 +4,29 @@ declare(strict_types = 1);
 
 namespace App\Tests\Module\Color\Domain;
 
+use LaSalle\ChupiProject\Module\CoolWord\Infrastructure\InMemoryCoolWordRepository;
+use LaSalle\ChupiProject\Module\CoolWord\Domain\RandomCoolWordSearcher;
 use PHPUnit\Framework\TestCase;
-
-namespace App\Tests\Module\CoolWord\Domain;
-
 
 final class RandomCoolWordSearcherTest extends TestCase
 {
+    private $coolWordRepository;
+    private $coolWordSearcher;
+    private $allCoolWords;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->coolWordRepository = new InMemoryCoolWordRepository();
+        $this->coolWordSearcher = new RandomCoolWordSearcher($this->coolWordRepository);
+        $this->allCoolWords = $this->coolWordRepository->all();
+    }
+
+    /** @test */
+    public function it_should_return_a_new_cool_word_from_the_repository()
+    {
+        $coolWord = $this->coolWordSearcher->__invoke();
+        $this->assertTrue(in_array($coolWord, $this->allCoolWords));
+    }
 
 }
