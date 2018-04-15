9a31bc5e0763c670b43d32ecb500340ff773847a
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-04-15 15:40:16 +0200

    Add Integration Test for InMemoryCoolWordRepository

---
+++ test/Module/CoolWord/Infrastructure/InMemoryCoolWordRepositoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Tests\Module\CoolWord\Infrastructure;
+
+use LaSalle\ChupiProject\Module\CoolWord\Infrastructure\InMemoryCoolWordRepository;
+use PHPUnit\Framework\TestCase;
+
+final class InMemoryCoolWordRepositoryTest extends TestCase
+{
+    /** @var InMemoryCoolWordRepository */
+    private $coolWordRepository;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->coolWordRepository = new InMemoryCoolWordRepository();
+    }
+
+    /** @test */
+    public function it_should_provide_an_array_of_colors()
+    {
+        $allCoolWords = $this->coolWordRepository->all();
+        $this->assertTrue(is_array($allCoolWords));
+    }
+}
